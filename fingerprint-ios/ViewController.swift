a367723a2cf72d06191f078ca5bf29e92c61e575
Donnie Mattingly <donniewmattingly@gmail.com>
2018-02-28 07:10:53 -0500

    Initial Commit

---
+++ fingerprint-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fingerprint-ios
+//
+//  Created by Donnie Mattingly on 2/28/18.
+//  Copyright © 2018 Donnie Mattingly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
